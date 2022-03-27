8629e9ca9337418f39c176ff58315b1efbb4f679
Danila Usachev <>
2022-03-27 14:35:11 +0300

    Introduced ExpressionParser API

---
+++ include/ast/ExpressionParser.hpp
@@ -0,0 +1,34 @@
+#ifndef CPMC_AST_EXPRESSION_PARSER_HPP_INCLUDED
+#define CPMC_AST_EXPRESSION_PARSER_HPP_INCLUDED
+
+#include "ast/Expression.hpp"
+#include "ast/ParsingContext.hpp"
+
+#include <memory>
+
+namespace cpmc {
+    namespace ast {
+
+        /**
+         * Parses CPM expressions.
+         *
+         * See Expression and ParsingContext.
+         */
+        class ExpressionParser {
+            private:
+                const ParsingContext& context;
+
+            public:
+                ExpressionParser(const ParsingContext& context);
+
+                /**
+                 * Parses next expression.
+                 *
+                 * @throws TODO
+                 */
+                std::unique_ptr<Expression> nextExpression();
+        };
+    }
+}
+
+#endif  // CPMC_AST_EXPRESSION_PARSER_HPP_INCLUDED include guard
---
+++ src/ast/ExpressionParser.cpp
@@ -0,0 +1,9 @@
+#include "ast/ExpressionParser.hpp"
+
+namespace cpmc {
+    namespace ast {
+
+        ExpressionParser::ExpressionParser(const ParsingContext& context):
+            context(context) {}
+    }
+}
